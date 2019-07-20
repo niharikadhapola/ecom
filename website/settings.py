b7a61480746d0958e99b7815ef6d50bcb92444fe
niharikadhapola <47639810+niharikadhapola@users.noreply.github.com>
2019-07-20 15:49:06 +0530

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ecomwebsite40@gmail.com'
-EMAIL_HOST_PASSWORD = 'ecom@1234'
+EMAIL_HOST_PASSWORD = '1234'
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
